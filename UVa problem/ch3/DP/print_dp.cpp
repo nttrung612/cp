95c12e705af1b365d27de3d9820c150eaa8bf663
nttrung612 <trung.nt225105@sis.hust.edu.vn>
2024-04-04 13:06:04 +0700

    print dp

---
+++ UVa problem/ch3/DP/print_dp.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int M, C;
+int price[25][25];
+int memo[210][25];
+
+int dp(int reM, int g) {
+    if(reM < 0) return -1e9; //if remaining money < 0 -> not solution -> return INF
+    if(g == C) return M-reM; //if buy all garments -> return (total Money - remaining money)
+    int &ans = memo[reM][g];
+    if(ans != -1) return ans; // if already calculated memo[reM][g]
+    for(int i=1;i<=price[g][0];++i) { //search all cases
+        ans = max(ans, dp(reM-price[g][i], g+1));
+    }
+    return ans;
+}
+
+vi ans;
+void print_dp(int g, int b) {
+    if(g == C || b < 0) {
+        for(int x : ans) cout << x << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i=1;i<=price[g][0];++i) {
+        if(dp(b-price[g][i], g+1) == dp(b, g)) {
+            ans.push_back(price[g][i]);
+            print_dp(g+1, b-price[g][i]);
+            ans.pop_back();
+        }
+    }
+}
+
+void solve() {
+    cin >> M >> C;
+    for(int i=0;i<C;++i) {
+        cin >> price[i][0];
+        for(int j=1;j<=price[i][0];++j) cin >> price[i][j];
+    }
+    memset(memo, -1, sizeof memo);
+    if(dp(M, 0) > 0) cout << dp(M,0) << '\n';
+    else cout << "no solution\n";
+    print_dp(0, M);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc=1;
+    //cin >> tc;
+    while(tc--)
+        solve();
+    return 0;
+}
